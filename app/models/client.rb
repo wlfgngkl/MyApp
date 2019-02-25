0186d0edaab137a21fb198dd5d5d32b5360fc148
wlfgngkl <wlfgngkl@gmail.com>
2019-02-25 17:24:33 +0100

    implement bootstrap

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,20 @@
 class Client < ApplicationRecord
+
+validates :first, :last, presence: {message: "Cant be blank!"}
+
+def fullname
+	first.capitalize +""+ last.capitalize
 end
+def title
+	if gender
+		if gender.eql? "m"
+			"Herr#{fullname}"
+		else
+			"Frau#{fullname}"
+		end
+	else
+		fullname
+	end
+
+ end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root to: "clients#index"
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
