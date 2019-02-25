f4a2adcdc7465e7c91416b7fb81e6a4432320f0c
wlfgngkl <wlfgngkl@gmail.com>
2019-02-25 21:08:06 +0100

    trying bootstrap

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,11 @@
 class Client < ApplicationRecord
 
-validates :first, :last, presence: {message: "Cant be blank!"}
+validates :firstname, :lastname, presence: {message: "Cant be blank!"}
 
 def fullname
-	first.capitalize +""+ last.capitalize
+	firstname.capitalize +""+ lastname.capitalize
 end
+
 def title
 	if gender
 		if gender.eql? "m"
