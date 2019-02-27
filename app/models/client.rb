0143f33d2df4b68c86a7097dc167fb43327ba883
wlfgngkl <wlfgngkl@gmail.com>
2019-02-27 11:41:41 +0100

    change dob, invisible notification/phone rename links, table caption germ new order fields 'new client'

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,20 +2,42 @@ class Client < ApplicationRecord
 
 validates :firstname, :lastname, presence: {message: "Cant be blank!"}
 
-def fullname
-	firstname.capitalize +""+ lastname.capitalize
-end
-
-def title
-	if gender
-		if gender.eql? "m"
-			"Herr#{fullname}"
-		else
-			"Frau#{fullname}"
-		end
-	else
-		fullname
-	end
-
+ def fullname
+	"#{firstname.capitalize} #{lastname.capitalize}"
  end
+
+ def title
+		if gender.eql? "m"
+			"Herr"
+		else
+			"Frau"		
+		end
+ end
+
+ def date_of_birth
+ 	dob.strftime("%d. #{month_names_ge(dob.mon)} %Y").html_safe
+ end
+
+ def gender_class
+ 	if gender.eql? "m"
+			"mister-blue"
+		else
+			"pink-lady"		
+		end
+ end
+
+
+ private
+
+ def month_names_ge(month)
+ 	months = ["Januar",
+ 						"Februar",
+ 						"M&auml;rz"
+
+ 	]
+
+ 	return months[month-1]
+ end
+
+
 end 
