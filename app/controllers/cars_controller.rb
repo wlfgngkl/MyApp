f9fdd649a8b506dee9a790d3b0a23f85deec4bbc
wlfgngkl <wlfgngkl@gmail.com>
2019-03-12 16:13:25 +0100

    changes landing-page

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -69,6 +69,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.require(:car).permit(:brand, :model, :color, :hp, :typeof)
+      params.require(:car).permit(:brand, :color, :hp, :typeof)
     end
 end
